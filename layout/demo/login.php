40bec8311f1062cbd883b1ce835dead6b9224e24
Frank Goodman <eckveldt@gmail.com>
2009-12-28 13:40:26 -0500

    Add isLoggedOn() functionality

--- layout/demo/login.php
+++ layout/demo/login.php
@@ -1,5 +1,8 @@
 <?php
 include "../User.php";
 $user = new User("mindbrane", "mindbrane");
+if ($user->isLoggedOn())
 print "My session cookie is: " . $user->getSessionID() . "\n";
+else
+print "You are not logged in.";
 ?>
