2fe433db0e923fc9a86c749fc6df413da8235c2f
Frank Goodman <eckveldt@gmail.com>
2009-12-28 13:16:09 -0500

    <pre> tags for readability

--- layout/demo/info.php
+++ layout/demo/info.php
@@ -2,7 +2,9 @@
 include "../User.php";
 $user = new User("mindbrane", "mindbrane");
 $prop = $user->getProp();
+echo "<pre>";
 print_r($prop);
 print $prop->name . "\n";
 print $prop->modhash . "\n";
+echo "</pre>";
 ?>
